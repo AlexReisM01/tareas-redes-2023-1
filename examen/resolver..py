2c2619562e71f3b398f72c385b613627cfe9579a
AlexanderR <areisenegger.2001@gmail.com>
2023-07-21 15:02:07 -0400

    a

--- examen/resolver..py
+++ examen/resolver..py
@@ -1,1 +1,9 @@
 import socket
+from dnslib import DNSRecord
+
+if __name__ == "__main__":
+    skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    skt.bind(("localhost", 8000))
+    while True:
+        data, address = skt.recvfrom(2048)
+        
