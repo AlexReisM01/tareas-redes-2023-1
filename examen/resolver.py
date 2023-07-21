73af4a9f4ebae850f93ed2d8adc30277a937b97a
AlexReisM01 <areisenegger.2001@gmail.com>
2023-07-21 16:22:36 -0400

    A

--- examen/resolver.py
+++ examen/resolver.py
@@ -1,5 +1,6 @@
 import socket
-from dnslib import DNSRecord
+from dnslib import DNSRecord, DNSHeader, DNSQuestion
+from dnslib.dns import RR, A
 
 if __name__ == "__main__":
     skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -7,7 +8,8 @@ if __name__ == "__main__":
     skt.settimeout(5)
     while True:
         try:
-            data, address = skt.recvfrom(2048)
-            print(data.decode())
+            data, address = skt.recvfrom(1024)
+            parsed_data = DNSRecord.parse(data)
+            print(parsed_data)
         except socket.timeout:
             print("timed out")
