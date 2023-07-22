3270e33b8a165f2d789b1fcae2913e4b5947c7bb
AlexanderR <areisenegger.2001@gmail.com>
2023-07-21 20:01:04 -0400

    dns ready

--- examen/resolver.py
+++ examen/resolver.py
@@ -1,7 +1,6 @@
 import socket
 from dnslib import DNSRecord, DNSHeader, DNSQuestion
 from dnslib.dns import RR, A, CLASS, QTYPE
-from example import print_dns_reply_elements
 
 ROOT_ADDRESS = ("192.33.4.12", 53)
 
@@ -29,24 +28,22 @@ class Record:
             rtype = QTYPE.get(ans.rtype)
             if(rtype == "A"):
                 return ans.rdata, rtype
-        return 0, ""
+        return '', ""
     
     def find_add(self):
         for add in self.add:
             rtype = QTYPE.get(add.rtype)
             if(rtype == "A"):
-                print(add.rdata)
-                return add.rdata, rtype
-        return 0, ""
+                return repr(add.rdata), rtype
+        return '', ""
     
     def find_auth(self):
         for auth in self.auth:
             
             rtype = QTYPE.get(auth.rtype)
-            print(f"looking for auth type: {rtype}")
             if(rtype == "NS"):
-                return auth.rdata, rtype
-        return 0, ""
+                return repr(auth.rdata), rtype
+        return '', ""
 
              
     
@@ -59,7 +56,6 @@ def resolver(query, address = ROOT_ADDRESS):
     sendskt.sendto(query, address)
     data, _ = sendskt.recvfrom(4096)
     parsed_data = DNSRecord.parse(data)
-    print_dns_reply_elements(parsed_data)
     ans_num, auth_num, ar_num = parsed_data.header.a, parsed_data.header.auth, parsed_data.header.ar
     record = Record(parsed_data.get_q().get_qname(), ans_num, auth_num, ar_num)
     if(ans_num>0):
@@ -72,13 +68,10 @@ def resolver(query, address = ROOT_ADDRESS):
     if rtype == "A":
         return data
     authanswer, authtype = record.find_auth()
-    print(authtype)
     if authtype == "NS":
         addanswer, addtype = record.find_add()
         if addtype == "A":
-            sendskt.sendto(query, (addanswer, 53))
-            adddata, _ = sendskt.recvfrom(4096)
-            return adddata
+            return resolver(query, (addanswer, 53))
         return resolver(query, (authanswer, 53))
     print("NO ANSWER")
     return None #ignore other type of answers
